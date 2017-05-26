0069b4b92c109502f76150fab4f173172cd652b3
csy10172003 <273027946@qq.com>
2017-05-26 15:12:29 +0800

    Signed-off-by: csy10172003 <273027946@qq.com>

---
+++ src/Test.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace phpcode;
+
+/**
+ * Monolog error handler
+ *
+ * A facility to enable logging of runtime errors, exceptions and fatal errors.
+ *
+ * Quick setup: <code>ErrorHandler::register($logger);</code>
+ *
+ * @author Jordi Boggiano <j.boggiano@seld.be>
+ */
+class test
+{ 
+    public static function log($msg){
+    //changyong1
+      echo $msg;
+    }
+}
